62fbdc726f67106efb16861df8fe06274c4a6f4c
mustafa <mltm0204@gmail.com>
2021-11-20 16:59:31 +0300

    codes are added

--- signup.js
+++ signup.js
@@ -3,56 +3,60 @@ const submitBtn = document.getElementById("submit");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
-window.onload = () =>{
+window.onload = () => {
     setTimeout(fillFormInput, 500);
 }
 
-const showLoading = () =>{
+const showLoading = () => {
     loading.style.display = "block";
 }
 
-const removeLoading = () =>{
+const removeLoading = () => {
     loading.style.display = "none";
 }
 
-const fillFormInput = () =>{
+const fillFormInput = () => {
     email.value = "eve.holt@reqres.in";
     password.value = "pistol";
     localStorage.setItem("email", EncryptStringAES(email.value));
     localStorage.setItem("password", EncryptStringAES(password.value));
 }
 
-submitBtn.addEventListener("click", (e) =>{
+submitBtn.addEventListener("click", (e) => {
     // e.preventDefault();
-    console.log("clicked");
+    // console.log("clicked");
     apiRegister();
 });
 
-const apiRegister = async ()=> {
+const apiRegister = async () => {
     const bodyData = {
-        email : email.value,
-        password : password.value
+        email: email.value,
+        password: password.value
     }
-    const response = await axios ({
-        url : "https://regres.in/api/register",
-        data : bodyData,
-        method : "post"
+    showLoading();
+    const response = await axios({
+        url: "https://reqres.in/api/register",
+        data: bodyData,
+        method: "post"
     })
-    .then(response => response.data)
-    .catch(error => {
-        alert(error);
-        showLoading();
-    });
-    if(response.token == undefined){
-        alert(response.error);
-        removeLoading();
-    }else{
-        removeLoading(;
-            localStorage.setItem
-    }
-    console.log(response.token);
-}
+        .then(response => response.data)
+        .catch(error => {
+            alert(error);
+            removeLoading();
+        });
 
+    if (response.token == undefined) {
+        alert("Undefined " + response.error);
+        removeLoading();
+    }
+    else {
+        removeLoading();
+        localStorage.setItem("tokenKey", response.token);
+        localStorage.setItem("tokenKeyEncrypted", EncryptStringAES(response.token));
+        removeLoading();
+        window.location.href = "userList.html";
+    }
+}
 
 // const apiRegister = async () => {
 //     const bodyData = {
@@ -61,20 +65,24 @@ const apiRegister = async ()=> {
 //     }
 //     console.log(bodyData);
 //     console.log(JSON.stringify(bodyData));
-//     await fetch("https://regres.in/api/register", {
+//     showLoading();
+//     fetch("https://reqres.in/api/register", {
 //         method : "POST",
 //         body : JSON.stringify(bodyData),
-//         hesaders : {"Content-Type" : "application/json"}
+//         headers : {"Content-Type": "application/json"}
 //     })
 //     .then((response) => response.json())
-//     .then((jsonResponse)=>{
+//     .then((jsonResponse) => {
 //         console.log(jsonResponse);
-//         if (jsonResponse.id ==undefined){
+//         console.log(jsonResponse.token);
+//         if (jsonResponse.token == undefined) {
 //             alert(jsonResponse.error);
 //             removeLoading();
-//         }else{
+//         }
+//         else{
 //             removeLoading();
-//             localStorage.setItem("tokenKey", jsonResponse);localStorage.setItem("tokenKeyEncrypyted", EncryptStringAES(jsonResponse.token));
+//             localStorage.setItem("tokenKey", jsonResponse.token);
+//             localStorage.setItem("tokenKeyEncrypted", EncryptStringAES(jsonResponse.token));
 //             // window.location.href = "userList.html";
 //         }
 //     })
@@ -83,4 +91,3 @@ const apiRegister = async ()=> {
 //         removeLoading();
 //     })
 // }
-
