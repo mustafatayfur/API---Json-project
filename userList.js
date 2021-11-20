98bbaffb810e51349b3558893e839ab5713e2b3e
mustafa <mltm0204@gmail.com>
2021-11-20 17:27:26 +0300

    first codes are added

--- userList.js
+++ userList.js
@@ -1,1 +1,21 @@
+const tbody = document.getElementById("tbodyUserList")
+window.onload = () => {
+    getApiUserList();
+}
+const getApiUserList = async () => {
+    const response = await axios ({
+        url :  "https://reqres.in/api/users?page=1",
+        method : "get"
+    });
 
+    console.log(response.data.data);
+    if (responseData.data.data[0].id == undefined){
+        alert("userList not found");
+
+    }else{
+        for (let index = 0; index<responseData.data.data.length; index++){
+
+        }
+
+    }
+}
