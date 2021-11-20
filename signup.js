d8b3e2f02f97ddaf302a7be4390ccfd1bbdeef14
mustafa <mltm0204@gmail.com>
2021-11-20 16:38:46 +0300

    apiRegister function is created

--- signup.js
+++ signup.js
@@ -28,32 +28,48 @@ submitBtn.addEventListener("click", (e) =>{
     apiRegister();
 });
 
-const apiRegister = async () => {
+
+const apiRegister = async ()=> {
     const bodyData = {
         email : email.value,
         password : password.value
     }
-    console.log(bodyData);
-    console.log(JSON.stringify(bodyData));
-    await fetch("https://regres.in/api/register", {
-        method : "POST",
-        body : JSON.stringify(bodyData),
-        hesaders : {"Content-Type" : "application/json"}
-    })
-    .then((response) => response.json())
-    .then((jsonResponse)=>{
-        console.log(jsonResponse);
-        if (jsonResponse.id ==undefined){
-            alert(jsonResponse.error);
-            removeLoading();
-        }else{
-            removeLoading();
-            localStorage.setItem("tokenKey", EncryptStringAES(jsonResponse.token));
-            window.location.href("userList.html")
-        }
-    })
-    .catch((error)=>{
-        alert(error);
-        removeLoading();
-    })
+    const response = await axios ({
+        url : "https://regres.in/api/register",
+        data : bodyData,
+        method : "post"
+    });
+    console.log(response);
 }
+
+
+// const apiRegister = async () => {
+//     const bodyData = {
+//         email : email.value,
+//         password : password.value
+//     }
+//     console.log(bodyData);
+//     console.log(JSON.stringify(bodyData));
+//     await fetch("https://regres.in/api/register", {
+//         method : "POST",
+//         body : JSON.stringify(bodyData),
+//         hesaders : {"Content-Type" : "application/json"}
+//     })
+//     .then((response) => response.json())
+//     .then((jsonResponse)=>{
+//         console.log(jsonResponse);
+//         if (jsonResponse.id ==undefined){
+//             alert(jsonResponse.error);
+//             removeLoading();
+//         }else{
+//             removeLoading();
+//             localStorage.setItem("tokenKey", jsonResponse);localStorage.setItem("tokenKeyEncrypyted", EncryptStringAES(jsonResponse.token));
+//             // window.location.href = "userList.html";
+//         }
+//     })
+//     .catch((error)=>{
+//         alert(error);
+//         removeLoading();
+//     })
+// }
+
